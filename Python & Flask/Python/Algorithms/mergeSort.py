d99961c3f65837aac9981cb306ac950ecb772da7
SumanRox <sumanrox@outlook.com>
2020-04-04 19:43:22 +0530

    Added Merge Sort

--- Python & Flask/Python/Algorithms/mergeSort.py
+++ Python & Flask/Python/Algorithms/mergeSort.py
@@ -1,49 +1,96 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr): 
-	if len(arr) >1: 
-		mid = len(arr)//2 #Finding the mid of the array 
-		L = arr[:mid] # Dividing the array elements 
-		R = arr[mid:] # into 2 halves 
+# Merge Sort Algorithm
+# pseudo code
+"""
 
-		mergeSort(L) # Sorting the first half 
-		mergeSort(R) # Sorting the second half 
+First Break the array into smaller array
 
-		i = j = k = 0
-		
-		# Copy data to temp arrays L[] and R[] 
-		while i < len(L) and j < len(R): 
-			if L[i] < R[j]: 
-				arr[k] = L[i] 
-				i+=1
-			else: 
-				arr[k] = R[j] 
-				j+=1
-			k+=1
-		
-		# Checking if any element was left 
-		while i < len(L): 
-			arr[k] = L[i] 
-			i+=1
-			k+=1
-		
-		while j < len(R): 
-			arr[k] = R[j] 
-			j+=1
-			k+=1
+mergeSort(arrray a)
+if (n==1)
+return a
 
-# Code to print the list 
-def printList(arr): 
-	for i in range(len(arr)):		 
-		print(arr[i],end=" ") 
-	print() 
+arrayOne=a[0]...a[n/2]
+arrayTwo=a[n/2+1]...a[n]
 
-# driver code to test the above code 
-if __name__ == '__main__': 
-	arr = [12, 11, 13, 5, 6, 7] 
-	print ("Given array is", end="\n") 
-	printList(arr) 
-	mergeSort(arr) 
-	print("Sorted array is: ", end="\n") 
-	printList(arr) 
+arrayOne=mergeSort(arrayOne)
+arrayTwo=mergeSort(arrayTwo)
 
-# This code is contributed by Mayank Khanna 
+return merge(arrayOne,arrayTwo)
+
+"""
+# Now after the array is borken down, lets merge the elements in 
+# a sorted manner
+"""
+
+merge(array a, array b)
+array c
+
+while (a and b have elements)
+if (a[0] > b[0])
+add b[0] to the end of c
+remove b[0] from b
+
+else
+add a[0] to the end of c
+remove a[0] from a
+
+// At this point either a or b is empty
+
+while (a has elements )
+add a[0] to the end of c
+
+while (b has elements)
+add b[0] to the end of c
+remove b[0] from b
+
+return c
+
+"""
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+from datetime import datetime
+a=datetime.now()
+myList = [54,26,93,17,77,31,44,55,20]
+mergeSort(myList)
+b=datetime.now()
+print(myList)
+print((b-a).total_seconds())
+
+        
+                
